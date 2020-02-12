a1c8402e19406995b0e3d5bb644790dd73c17dea
Anastasiya Popova <Anastasiya@Anastasiyas-MacBook-Pro.local>
2020-02-12 22:07:41 +0300

    feat: add interface searhResponse and searchItem

---
+++ src/app/models/search-item.model.ts
@@ -0,0 +1,42 @@
+interface Thumbnail {
+    url: string,
+    width: number, 
+    height: number
+}
+
+interface Snippet {
+    publishedAt: string,
+    channelId: string,
+    title: string,
+    description: string,
+    thumbnails: {
+        default: Thumbnail,
+        medium: Thumbnail,
+        high: Thumbnail,
+        standard: Thumbnail,
+        maxres: Thumbnail
+    },
+    channelTitle: string,
+    tags: string[],
+    categoryId: string,
+    liveBroadcastContent: string,
+    localized: {
+        title: string,
+        description: string
+    },
+    defaultAudioLanguage: string
+}
+
+export default interface SearchItem {
+    kind: string,
+    etag: string,
+    id: string,
+    snippet: Snippet,
+    statistics: {
+        viewCount: string,
+        likeCount: string,
+        dislikeCount: string,
+        favoriteCount: string,
+        commentCount: string
+    }
+}
---
+++ src/app/models/search-response.model.ts
@@ -0,0 +1,11 @@
+import SearchItem from './search-item.model';
+
+export default interface SearchResponse {
+    kind: string,
+    etag: string,
+    pageInfo: {
+        totalResults: number,
+        resultsPerPage: number
+    },
+    items: SearchItem[]
+}
